6db0634c10c65ab53aa85ca7b4c76a18378df563
Aditya <adityajais8877@gmail.com>
2022-09-18 19:04:25 +0530

    initial commit

--- DS_ALGO_JS/Commin3Sorted.js
+++ DS_ALGO_JS/Commin3Sorted.js
@@ -5,21 +5,21 @@ let n1=arr1.length;
 let n2=arr2.length;
 let n3=arr3.length;
 let i=0,j=0,k=0;
-while (i < n1 && j < n2 && k < n3) {
+
+while (i<n1 && j<n2 && k<n3) {
     if (arr1[i] == arr2[j] && arr2[j] == arr3[k]) {
         console.log(arr1[i]);
-        i++; j++; k++;
+        i++; 
+        j++; 
+        k++;
     }
-
     else if (arr1[i] < arr2[j]) {
         i++;
     }
     else if (arr2[j] < arr3[k]) {
         j++; 
     }
- 
     else {
         k++; 
     }
 }
-
